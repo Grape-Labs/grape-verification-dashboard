7b55b35d79939135a254290376af9dcbd6f4236b
Kirk <kfournia@gmail.com>
2023-03-23 19:46:34 +0200

    Minor update

--- src/components/Tools/constants.ts
+++ src/components/Tools/constants.ts
@@ -1,10 +1,10 @@
 import STATIC_LOGO from '../../public/grape_white_logo.svg';
 import RAYDIUM_STATIC_LOGO from '../../public/logos/platforms/raydium.png';
 export const GRAPE_APP_API_URL = process.env.REACT_APP_API_URL || null;
-export const TX_RPC_ENDPOINT = process.env.REACT_APP_API_FREE_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
+export const TX_RPC_ENDPOINT = process.env.REACT_APP_API_GRAPE_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
 export const GRAPE_RPC_ENDPOINT = process.env.REACT_APP_API_GRAPE_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
 //export const GOVERNANCE_RPC_ENDPOINT = process.env.REACT_APP_API_GOVERNANCE_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
-export const GOVERNANCE_RPC_ENDPOINT = process.env.REACT_APP_API_THEINDEX_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
+export const GOVERNANCE_RPC_ENDPOINT = process.env.REACT_APP_API_GRAPE_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
 export const BONFIDA_TWITTER_REGISTRAR_URL = process.env.REACT_APP_API_BONFIDA_TWITTER_REGISTRAR_SERVER_URL || null;
 export const GRAPE_TREASURY = 'GrapevviL94JZRiZwn2LjpWtmDacXU8QhAJvzpUMMFdL';
 
