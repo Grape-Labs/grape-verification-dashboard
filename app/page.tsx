ac78f7731260925a0fd9682097a733e0a5214d76
Kirk <kfournia@gmail.com>
2026-02-27 09:41:12 +0200

    updatd logo

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
   Divider,
   IconButton,
   Paper,
+  SvgIcon,
   Stack,
   Tooltip,
   Typography,
@@ -259,32 +260,50 @@ function platformLabel(platform: PlatformKey): string {
   }
 }
 
-function platformIcon(platform: PlatformKey) {
-  const logo = (src: string, alt: string) => (
-    <Box
-      component="img"
-      src={src}
-      alt={alt}
-      sx={{
-        width: 18,
-        height: 18,
-        display: "block",
-        objectFit: "contain",
-      }}
-    />
+function DiscordBrandIcon({
+  sx,
+}: {
+  sx?: React.ComponentProps<typeof SvgIcon>["sx"];
+}) {
+  return (
+    <SvgIcon viewBox="0 0 40 40" sx={sx}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M25.71 28.15C30.25 28 32 25.02 32 25.02c0-6.61-2.96-11.98-2.96-11.98-2.96-2.22-5.77-2.15-5.77-2.15l-.29.32c3.5 1.07 5.12 2.61 5.12 2.61a16.75 16.75 0 0 0-10.34-1.93l-.35.04a15.43 15.43 0 0 0-5.88 1.9s1.71-1.63 5.4-2.7l-.2-.24s-2.81-.07-5.77 2.15c0 0-2.96 5.37-2.96 11.98 0 0 1.73 2.98 6.27 3.13l1.37-1.7c-2.6-.79-3.6-2.43-3.6-2.43l.58.35.09.06.08.04.02.01.08.05a17.25 17.25 0 0 0 4.52 1.58 14.4 14.4 0 0 0 8.3-.86c.72-.27 1.52-.66 2.37-1.21 0 0-1.03 1.68-3.72 2.44.61.78 1.35 1.67 1.35 1.67Zm-9.55-9.6c-1.17 0-2.1 1.03-2.1 2.28 0 1.25.95 2.28 2.1 2.28 1.17 0 2.1-1.03 2.1-2.28.01-1.25-.93-2.28-2.1-2.28Zm7.5 0c-1.17 0-2.1 1.03-2.1 2.28 0 1.25.95 2.28 2.1 2.28 1.17 0 2.1-1.03 2.1-2.28 0-1.25-.93-2.28-2.1-2.28Z"
+      />
+    </SvgIcon>
   );
+}
 
+function TelegramBrandIcon({
+  sx,
+}: {
+  sx?: React.ComponentProps<typeof SvgIcon>["sx"];
+}) {
+  return (
+    <SvgIcon viewBox="0 0 32 32" sx={sx}>
+      <path
+        fill="currentColor"
+        d="M7.034 15.252c4.975-2.167 8.293-3.596 9.953-4.287 4.74-1.971 5.725-2.314 6.366-2.325.142-.002.457.033.662.198.172.14.22.33.243.463.022.132.05.435.028.671-.257 2.7-1.368 9.248-1.933 12.27-.24 1.28-.71 1.708-1.167 1.75-.99.091-1.743-.655-2.703-1.284-1.502-.985-2.351-1.598-3.81-2.558-1.684-1.11-.592-1.721.368-2.718.252-.261 4.619-4.233 4.703-4.594.01-.045.02-.213-.08-.301-.1-.09-.246-.059-.353-.035-.15.034-2.55 1.62-7.198 4.758-.682.468-1.298.696-1.851.684-.61-.013-1.782-.344-2.653-.628-1.069-.347-1.918-.53-1.845-1.12.039-.308.462-.623 1.27-.944Z"
+      />
+    </SvgIcon>
+  );
+}
+
+function platformIcon(platform: PlatformKey) {
   switch (platform) {
     case "discord":
-      return logo("/discord-logo.svg", "Discord logo");
+      return <DiscordBrandIcon sx={{ fontSize: 18 }} />;
     case "telegram":
-      return logo("/telegram-logo.svg", "Telegram logo");
+      return <TelegramBrandIcon sx={{ fontSize: 18 }} />;
     case "twitter":
       return <XIcon sx={{ fontSize: 18 }} />;
     case "email":
       return <EmailIcon sx={{ fontSize: 18 }} />;
     default:
-      return logo("/discord-logo.svg", "Discord logo");
+      return <DiscordBrandIcon sx={{ fontSize: 18 }} />;
   }
 }
 
